fa399da166c2206b7f87eb49fe5aa4e7a6b10058
sammyrock12 <92696638+sammyrock12@users.noreply.github.com>
2021-11-03 18:55:51 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
+//Choose the correct option by uncommenting the right line to load the animation for ship.
+  //shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
+  //shipImg1 = loadAnimation("ship-1","ship-1","ship-2","ship-1");
+  //shipImg1 = loadAnimation("1.png","2.png","3.png","4.png");
+
+}
+
+function setup(){
+  createCanvas(600,500);
+  background("blue");
+
+  // Moving background
+  sea=createSprite(600,300);
+  sea.addImage(seaImg);
+  sea.scale=0.5;
+  sea.velocityX = -3;
+  
+  ship = createSprite(50,200,20,50);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  ship.x=90;
+  ship.y=300;
+}
+
+function draw() {
+  background(0);
+  
+//Uncomment the correct line to make the background move to create a forward moving effect for the boat.
+  //sea.velocityY = -3;
+  //sea.x = -3;
+  //sea.velocityX = -3;
+  //sea.velocityX = 3;
+  
+
+  
+    
+  drawSprites();
+}
